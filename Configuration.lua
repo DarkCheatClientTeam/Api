14dc966a2101b885cbf30ccecb581cb3d184a313
Gregory909 <124597526+DarkCheatClientTeam@users.noreply.github.com>
2026-01-03 17:58:55 +0300

    Update Configuration.lua

--- Configuration.lua
+++ Configuration.lua
@@ -1,1 +1,25 @@
+local API = {};
+local HardBans = {"1293792120","1993687031","604430770","1222680","6225378520","7166779222"};
+local IPBans = {};
+local Warnings = {["7394266117"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9984847441"] = {Reason = "Off-topic Report",	Moderator = "Gregory909"}, ["9021590842"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9824352500"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9528456159"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9707598727"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["8564588441"] = {Reason = "Off-topic Report",Moderator = "Gregory909"}};
+function API:GetHardBans()
+	return HardBans;
+end;
+function API:GetIPBans()
+	return IPBans;
+end;
+function API:GetWarnings()
+	return Warnings;
+end;
+function API:IsHardBanned(player)
+	return table.find(HardBans, tostring(player.UserId)) ~= nil;
+end;
+function API:HasWarning(userId)
+	return Warnings[tostring(userId)] ~= nil;
+end;
 
+function API:IsIPBanned(ip)
+	return table.find(IPBans, tostring(ip)) ~= nil;
+end;
+
+return API;
