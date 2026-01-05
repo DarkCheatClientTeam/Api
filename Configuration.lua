3c59a8dbe6aeb420591311c25dbf4e2c94193623
Gregory909 <124597526+DarkCheatClientTeam@users.noreply.github.com>
2026-01-05 09:55:13 +0500

    Update Configuration.lua

--- Configuration.lua
+++ Configuration.lua
@@ -1,7 +1,7 @@
 local API = {};
 local HardBans = {"1293792120","1993687031","604430770","1222680","6225378520","7166779222"};
 local IPBans = {};
-local Warnings = {["7394266117"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9984847441"] = {Reason = "Off-topic Report",	Moderator = "Gregory909"}, ["9021590842"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9824352500"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9528456159"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9707598727"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["8564588441"] = {Reason = "Off-topic Report",Moderator = "Gregory909"}};
+local Warnings = {["10200076979"] = {Reason = "Off-topic Report + N-words",Moderator = "Gregory909"},["7394266117"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9984847441"] = {Reason = "Off-topic Report",	Moderator = "Gregory909"}, ["9021590842"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9824352500"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9528456159"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["9707598727"] = {Reason = "Off-topic Report",Moderator = "Gregory909"},["8564588441"] = {Reason = "Off-topic Report",Moderator = "Gregory909"}};
 function API:GetHardBans()
 	return HardBans;
 end;
