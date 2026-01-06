78737afa38257690a75b884fe35d3862bae4e0aa
Gregory909 <124597526+DarkCheatClientTeam@users.noreply.github.com>
2026-01-06 21:53:38 +0300

    Update Configuration.lua

--- Configuration.lua
+++ Configuration.lua
@@ -1,7 +1,7 @@
 local API = {};
 local HardBans = {"1293792120","1993687031","604430770","1222680","6225378520","7166779222","10035014576"};
 local IPBans = {};
-local Warnings = {"8564588441":{"Moderator":"Gregory909","Reason":"Off-topic Report"},"9707598727":{"Moderator":"Gregory909","Reason":"Off-topic Report"},"9021590842":{"Moderator":"Gregory909","Reason":"Off-topic Report"},"10200076979":{"Moderator":"Gregory909","Reason":"Off-topic Report + N-words"},"9528456159":{"Moderator":"Gregory909","Reason":"Off-topic Report"},"7394266117":{"Moderator":"Gregory909","Reason":"Off-topic Report"},"9824352500":{"Moderator":"Gregory909","Reason":"Off-topic Report"},"9984847441":{"Moderator":"Gregory909","Reason":"Off-topic Report"}};
+local Warnings = {};
 function API:GetHardBans()
 	return HardBans;
 end;
